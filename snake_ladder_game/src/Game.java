7f35fe3c93f2280c6767186d6cfed56760a9d6be
Aniketsomkuwar <aniketsomkuwar1101@gmail.com>
2021-11-21 00:33:20 +0530

    board attached

--- snake_ladder_game/src/Game.java
+++ snake_ladder_game/src/Game.java
@@ -1,33 +1,68 @@
+import java.util.Scanner;
 
 public class Game {
 
     public static int rollDice() {
-        return (int) (Math.random() * 7);
+        int n = (int) (Math.random() * 7);
+        return (n == 0 ? 1 : n);
+
     }
 
-
-    public static void main(String[] args) {
-
+    public static int[][] Board() {
         int[][] board = new int[10][10];
 
-        int count = 1;
+        int count = 100;
         for (int i = 0; i < board.length; i++) {
-
-            for (int j = 0; j < board.length; j++) {
-
-                board[i][j] = count++;
+            if (i % 2 == 0) {
+                for (int j = 0; j < board.length; j++) {
+                    board[i][j] = count--;
+                }
+            } else {
+                for (int j = board.length - 1; j > -1; j--) {
+                    board[i][j] = count--;
+                }
             }
         }
+        return board;
+    }
 
-
+    public static void DisplayBoard(int[][] board, Player p1, Player p2) {
         for (int i = 0; i < board.length; i++) {
-
             for (int j = 0; j < board.length; j++) {
-
-                System.out.print(board[i][j] + "   ");
+                if (p1.getPlayerPosition() == p2.getPlayerPosition() && p1.getPlayerPosition() > 1) {
+                    System.out.println(board[i][j] + "<- both ");
+                    continue;
+                }
+                if (p1.getPlayerPosition() == board[i][j] && p1.getPlayerPosition() > 1) {
+                    System.out.print(board[i][j] + "<-P ");
+                } else if (p2.getPlayerPosition() == board[i][j] && p1.getPlayerPosition() > 1) {
+                    System.out.print(board[i][j] + "<-C ");
+                } else {
+                    System.out.print(board[i][j] + "   ");
+                }
             }
             System.out.println();
         }
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("welcome to Snake and Ladder Game");
+//        System.out.println("enter player name");
+
+        Player player1 = new Player("Aniket", 'P');
+        Player player2 = new Player("Computer", 'C');
+
+        int[][] board = Board();
+        player1.setPlayerPosition(20);
+        player2.setPlayerPosition(47);
+        DisplayBoard(board, player1, player2);
+//       while(player1.getPlayerPosition()!=100 && player2.getPlayerPosition()!=100){
+//
+//
+//       }
+
 
     }
 }
--- snake_ladder_game/src/Player.java
+++ snake_ladder_game/src/Player.java
@@ -1,25 +1,55 @@
 class Player {
-    String name = "";
-    int PlayerNo = 0;
+    String PlayerName = "";
 
-    public Player(String name, int playerNo) {
-        this.name = name;
-        PlayerNo = playerNo;
+    char PlayerSymbol = '0';
+
+    int PlayerPosition = 1;
+    int PLayerX = 0;
+    int PlayerY = 0;
+
+    public Player(String playerName, char playerSymbol) {
+        PlayerName = playerName;
+        PlayerSymbol = playerSymbol;
     }
 
-    public String getName() {
-        return name;
+    public int getPlayerPosition() {
+        return PlayerPosition;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setPlayerPosition(int playerPosition) {
+        PlayerPosition = playerPosition;
     }
 
-    public int getPlayerNo() {
-        return PlayerNo;
+    public int getPLayerX() {
+        return PLayerX;
     }
 
-    public void setPlayerNo(int playerNo) {
-        PlayerNo = playerNo;
+    public void setPLayerX(int PLayerX) {
+        this.PLayerX = PLayerX;
     }
+
+    public int getPlayerY() {
+        return PlayerY;
+    }
+
+    public void setPlayerY(int playerY) {
+        PlayerY = playerY;
+    }
+
+    public char getPlayerSymbol() {
+        return PlayerSymbol;
+    }
+
+    public void setPlayerSymbol(char playerSymbol) {
+        PlayerSymbol = playerSymbol;
+    }
+
+    public String getPlayerName() {
+        return PlayerName;
+    }
+
+    public void setPlayerName(String playerName) {
+        PlayerName = playerName;
+    }
+
 }
